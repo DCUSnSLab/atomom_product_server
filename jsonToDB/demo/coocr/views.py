411cea8284581f55e80a37379333d9016a0f868f
sujong kim <dgdgksj@gmail.com>
2022-01-07 15:42:58 +0900

    feat: update current html soure
    하나의 이미지 처리가 가능하게 기능을 개선하였습니다
    이제 html 페이지를 조금 꾸며볼 생각입니다

--- jsonToDB/demo/coocr/views.py
+++ jsonToDB/demo/coocr/views.py
@@ -1,12 +1,19 @@
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
-import os
+import os, sys
 
 from PIL import Image
-import pytesseract
-
-pytesseract.pytesseract.tesseract_cdm = "C:/Program Files/Tesseract-OCR/tesseract.exe"
-
+curPath=os.getcwd()
+path=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
+print(path)
+# print(os.listdir(path))
+path=os.path.join(path,'atoOCR')
+print(path)
+sys.path.append(path)
+import demo_modifed_for_one_image_processing as ocr
+os.chdir(path)
+craftModel, model, opt = ocr.setModel()
+os.chdir(curPath)
 def home(request):
     context = {}
     context['menutitle'] = 'HOME'
@@ -30,11 +37,23 @@ def coocr_upload(request):
             fs = FileSystemStorage(location='static/source')
             imgname = fs.save(f"src-{name_old}", uploadfile)
 
+
             imgfile = Image.open(f"./static/source/{imgname}")
-            resulttext = pytesseract.image_to_string(imgfile, lang='kor')
+            imgPath=curPath+f"./static/source/{imgname}"
+
+            os.chdir(path)
+            img, points = ocr.craftOperation(imgPath, craftModel, dirPath=opt.image_folder)
+            texts = ocr.demo(opt,model)
+            img = ocr.putText(img, points, texts)
+            image = img
+            ocr.mkdir()
+
+            resulttext = texts
+            os.chdir(curPath)
+
 
     context['imgname'] = imgname
-    context['resulttext'] = resulttext.replace(" ","")
+    context['resulttext'] = resulttext
     return render(request, 'coocr_upload.html', context)
 
 
