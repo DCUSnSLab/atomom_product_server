4743ece3cb8e269f1ca2b1397fa27d1a4d6420cf
sujong kim <dgdgksj@gmail.com>
2022-01-10 22:40:56 +0900

    feat: text clsutering
    기존 방식은 바운딩 박스 각각을 하나의 단어로 봤으나 이러면
    토피넛 라떼의 경우
    토피넛
    라떼
    위와 같이 하나의 덩어리이나 각각 별개로 분리되버립니다
    따라서 바운딩박스의 중심값 기준으로 바운딩 박스를 임계값에 따라 그룹화를
    진행합니다.
    그 후 그룹들을 열 기준으로 재정렬합니다
    정렬된 리스트를 다시 설정한 거리 값에 따라 그룹화를 진행합니다
    이러면 위의 예시와 달리 토피넷 라떼 이렇게 한 덩어리로 묶입니다
    관련한 논문을 찾지 못해서 임의로 생각한대로 구현하였습니다
    차후 에러 발생시 수정하겠습니다

--- jsonToDB/demo/coocr/views.py
+++ jsonToDB/demo/coocr/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 import os, sys
 
 from PIL import Image
+import cv2
 curPath=os.getcwd()
 path=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 print(path)
@@ -20,6 +21,104 @@ def home(request):
 
     return render(request, 'home.html', context)
 
+def groupby(points,texts,img):
+    rows,cols,_ =img.shape
+    rThres = int(rows / 100)
+    cThres = int(cols / 20)
+    # newPoint
+
+    datas=[]
+    #포인트들에 텍스트 추가 (1,3,2,3,타이레놀)
+    for i, point in enumerate(points):
+        # print(i,point,texts[i])
+        r1, c1, r2, c2 = point
+        data=(r1,c1,r2,c2,texts[i])
+        datas.append(data)
+
+    check=False
+    br=0
+    splitIndex=[]
+    for i, data in enumerate(datas):
+        r1, c1,r2,c2,t = data
+        if(check==False):
+            br=r1
+            check=True
+            continue
+        if(rThres<int(r1-br)):
+            splitIndex.append(i)
+        else:
+            pass
+        br=r1
+    newTexts=""
+
+    b=0
+    newDatas=[]
+    for i in range(len(splitIndex)):
+        rowi=i
+        i=splitIndex[i]
+        data=datas[b:i]
+
+        data = sorted(data, key=lambda x: (x[1]))
+        b=i
+        for i in data:
+            newDatas.append(i)
+        if(rowi==len(splitIndex)-1):
+            # print(len(datas),i)
+            data = datas[b:len(datas)]
+            # print(data)
+            # print(data)
+            data = sorted(data, key=lambda x: (x[1]))
+            for i in data:
+                newDatas.append(i)
+    print("-"*50)
+    print(newDatas)
+    newTexts=""
+    check=False
+    bdata=0
+    # print(img.shape)
+    centroid=0
+    bcentroid=0
+    rThres = int(rows / 30)
+    for i, data in enumerate(newDatas):
+        if(check==False):
+            check=True
+            bdata=data
+            br1, bc1, br2, bc2, t = bdata
+            newTexts+=t
+            continue
+        br1, bc1, br2, bc2, t = bdata
+        r1, c1, r2, c2, t = data
+        bcentroid=abs(br2-br1)+br1
+        centroid=abs(r2-r1)+r1
+        print(bdata, data)
+        print("     bcentroid,centroid, rThres, int(abs(bcentroid-centroid))",bcentroid,centroid,rThres,int(abs(bcentroid-centroid)))
+        if(rThres>int(abs(bcentroid-centroid))):
+
+            print("     ",cThres,abs(int(c1-bc2)))
+            if(cThres>abs(int(c1-bc2))):
+                newTexts+=' '
+            else:
+                newTexts += '\n'
+        else:
+            newTexts+='\n'
+        newTexts+=t
+        bdata=data
+    print(newTexts)
+    return newTexts
+
+
+    #
+    # for i, data in enumerate(newDatas):
+    #     r1, c1, r2, c2, t = data
+    #     newTexts+=t
+    #     if i in splitIndex:
+    #         newTexts+='\n'
+    # print("-" * 50)
+    # print(newTexts)
+    # print("-" * 50)
+
+
+
 
 def coocr_upload(request):
     context = {}
@@ -27,6 +126,7 @@ def coocr_upload(request):
 
     imgname = ''
     resulttext = ''
+    parsedText=''
     if 'uploadfile' in request.FILES:
         uploadfile = request.FILES.get('uploadfile', '')
 
@@ -36,6 +136,9 @@ def coocr_upload(request):
 
             fs = FileSystemStorage(location='static/source')
             imgname = fs.save(f"src-{name_old}", uploadfile)
+            # print(uploadfile)
+            # print(type(uploadfile))
+            # print(imgname)
 
 
             imgfile = Image.open(f"./static/source/{imgname}")
@@ -44,16 +147,60 @@ def coocr_upload(request):
             os.chdir(path)
             img, points = ocr.craftOperation(imgPath, craftModel, dirPath=opt.image_folder)
             texts = ocr.demo(opt,model)
-            img = ocr.putText(img, points, texts)
-            image = img
+            # print(points)
+            # print(texts)
+            parsedText=groupby(points,texts,img)
+            # img = ocr.putText(img, points, texts)
             ocr.mkdir()
 
             resulttext = texts
             os.chdir(curPath)
+            # print(f"{imgname}")
+            # print(imgname)
+            name, ext = os.path.splitext(imgname)
+            # print('name :', name)
+            # print('ext :', ext)
+            resultImgname=name+"_result"+ext
+
+            cv2.imwrite(curPath+f"./static/source/{resultImgname}",img)
 
 
     context['imgname'] = imgname
-    context['resulttext'] = resulttext
+    context['resultImgname'] = resultImgname
+    resulttext='\n'.join(resulttext)
+    print(type(resulttext))
+    # context['resulttext'] = '\n'+resulttext
+    context['resulttext'] = '\n' + parsedText
     return render(request, 'coocr_upload.html', context)
+
+
+# def coocr_upload(request):
+#     context = {}
+#     context['menutitle'] = 'OCR READ'
+#
+#     imgname = ''
+#     resulttext = ''
+#     if 'uploadfile' in request.FILES:
+#         uploadfile = request.FILES.get('uploadfile', '')
+#
+#         if uploadfile != '':
+#             name_old = uploadfile.name
+#             name_ext = os.path.splitext(name_old)[1]
+#
+#             fs = FileSystemStorage(location='static/source')
+#             imgname = fs.save(f"src-{name_old}", uploadfile)
+#
+#             imgfile = Image.open(f"./static/source/{imgname}")
+#             import pytesseract
+#             resulttext = pytesseract.image_to_string(imgfile, lang='kor')
+#
+#     context['imgname'] = imgname
+#     print(resulttext)
+#     print(type(resulttext))
+#     context['resulttext'] = resulttext.replace(" ","")
+#     print(context['resulttext'])
+#     print(type(context['resulttext']))
+#
+#     return render(request, 'coocr_upload.html', context)
 
 
